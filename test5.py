cd755ab0eadb2765e60b23ca40a1e5897faf0b52
shawon <md.shawon.islam@ulkasemi.com>
2022-01-19 19:00:23 +0600

    added impdata

--- test5.py
+++ test5.py
@@ -5,12 +5,29 @@ with open("test5.sv") as f:
 f.close()
 
 direction=["inout","input","output"]
-
+impData=[]
 for eachLine in lines:
-    stripedLine=eachLine.strip()
-    if stripedLine.startswith("module"):
-        speciality=specialityCheck(stripedLine)
-        break
-print(speciality)
-if speciality["onlyParams"]:
-    info=infoForArgumentDeclartion(lines)
+    if eachLine.startswith("module"):
+        brac_splited=eachLine.split("(")[1]
+        commaSplited=brac_splited.split(",")
+        for elem in commaSplited:
+            impData.append(elem)
+        continue
+    commaSplited=eachLine.split(",")
+    print(commaSplited)
+    for elem in commaSplited:
+        print(elem)
+        semiSplited=eachLine.split(";")
+        print(semiSplited)
+
+
+# for eachLine in lines:
+#     stripedLine=eachLine.strip()
+#     if stripedLine.startswith("module"):
+#         speciality=specialityCheck(stripedLine)
+#         break
+
+# if speciality["onlyParams"]:
+#     info=infoForArgumentDeclartion(lines)
+# if not speciality["singleLined"]:
+#     print("hello ")
