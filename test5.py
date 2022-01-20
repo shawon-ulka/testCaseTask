3e26f84fe91a9a4e78fa4f6921dff237d53b9c8e
shawon <md.shawon.islam@ulkasemi.com>
2022-01-20 11:17:35 +0600

    extract only the important element

--- test5.py
+++ test5.py
@@ -1,5 +1,7 @@
+from gettext import find
 from utils import specialityCheck
 from utils import infoForArgumentDeclartion
+import re
 with open("test5.sv") as f:
     lines=f.readlines()
 f.close()
@@ -11,23 +13,19 @@ for eachLine in lines:
         brac_splited=eachLine.split("(")[1]
         commaSplited=brac_splited.split(",")
         for elem in commaSplited:
+            if elem=="" or elem=="\n":
+                continue
             impData.append(elem)
         continue
-    commaSplited=eachLine.split(",")
-    print(commaSplited)
-    for elem in commaSplited:
-        print(elem)
-        semiSplited=eachLine.split(";")
-        print(semiSplited)
-
-
-# for eachLine in lines:
-#     stripedLine=eachLine.strip()
-#     if stripedLine.startswith("module"):
-#         speciality=specialityCheck(stripedLine)
-#         break
-
-# if speciality["onlyParams"]:
-#     info=infoForArgumentDeclartion(lines)
-# if not speciality["singleLined"]:
-#     print("hello ")
+    if eachLine.startswith("endmodule"):
+        continue
+    eachLine=eachLine.strip()
+    if eachLine=="" or eachLine=="\n":
+        continue
+    splitted=re.split('[,;]+', eachLine)
+    for elem in splitted:
+        elem=elem.strip()
+        elem=elem.replace(")","")
+        if elem=="" or elem=="\n":
+            continue
+        impData.append(elem)
--- test5.sv
+++ test5.sv
@@ -1,5 +1,6 @@
 module frmt5(input a, output reg f,y,
 input wire b,d,
+inout clk;output reg name1,name2;
 output reg c);
 
 endmodule
